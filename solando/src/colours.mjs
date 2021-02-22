849cee671c0b6c72c0dcd70bfa7e275f74c27259
Rob Eady <robertj.eady@gmail.com>
2021-02-22 01:57:02 +0000

    delete old colours

--- solando/src/colours.mjs
+++ solando/src/colours.mjs
@@ -40,31 +40,6 @@ const bright = {
   yellow: normal.yellow.lighten(0.25).saturate(0.1)
 }
 
-// original colors (they are probably garbage)
-// const normal = {
-//   black: color('#282A2E'),
-//   blue: color('#5F819D'),
-//   cyan: color('#5E8D87'),
-//   green: color('#8C9440'),
-//   magenta: color('#85678F'),
-//   red: color('#A54242'),
-//   white: color('#707880'),
-//   yellow: color('#DE935F'),
-//   // Extras
-//   orange: color('#cc6953'),
-// }
-
-// const bright = {
-//   black: color('#373B41'),
-//   blue: color('#81A2BE'),
-//   cyan: color('#8ABEB7'),
-//   green: color('#B5BD68'),
-//   magenta: color('#B294BB'),
-//   red: color('#CC6666'),
-//   white: color('#C5C8C6'),
-//   yellow: color('#F0C674'),
-// }
-
 const foreground = color.hsl(220, 10, 10)
 
 export {
